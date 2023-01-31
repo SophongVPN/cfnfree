8003b658b9f9dafdb117cf3a169945104547fcfa
SophongVPN <83055091+SophongVPN@users.noreply.github.com>
2023-01-31 11:41:20 +0700

    Update edu.sh

--- websocket/edu.sh
+++ websocket/edu.sh
@@ -47,6 +47,8 @@ CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
 ExecStart=/usr/bin/python -O /usr/local/bin/ws-nontls 8880
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-nontls 80
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-nontls 8280
 Restart=on-failure
 
 [Install]
